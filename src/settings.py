ccffc9ac2c8110c908f6a36ac99afd6fc60da4c1
Cadu de Castro Alves <cadudecastroalves@gmail.com>
2012-03-11 17:11:55 -0300

    added index.html

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,7 @@
 # Django settings for src project.
+import os
+
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -106,6 +109,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_ROOT, 'templates')
 )
 
 INSTALLED_APPS = (
--- src/urls.py
+++ src/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'src.views.home', name='home'),
+    url(r'^$', 'core.views.index', name='index'),
     # url(r'^src/', include('src.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
